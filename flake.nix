37f674d9a80d331bd8142e10cc30b3eb7a0f32b7
Peter Esselius <peter.esselius@pagero.com>
2021-04-16 08:28:00 +0200

    clean clean clean

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
         system = "x86_64-darwin";
       };
 
-      mkDenoDrv = { name, lockfile, entrypoint }:
+      mkDenoDrv = { name, lockfile, entrypoint }@args:
         let
           inherit (pkgs) lib fetchurl linkFarm writeText runCommand deno;
           inherit (pkgs.lib) elemAt flatten mapAttrsToList importJSON;
@@ -14,20 +14,22 @@
           artifactPath = url: "${urlPart url 0}/${urlPart url 1}/${builtins.hashString "sha256" (urlPart url 2)}";
           file = name: content: { inherit name; path = content; };
 
-          dependency = lock: flatten (mapAttrsToList
+          lockData = importJSON lockfile;
+
+          depsFromLockData = flatten (mapAttrsToList
             (url: sha256: [
               (file "${artifactPath url}" (fetchurl { inherit url sha256; }))
               (file "${artifactPath url}.metadata.json" (writeText "metadata.json" ''{"headers": {}, "url": ""}''))
             ])
-            lock);
+            lockData);
 
-          deps = lockfile: linkFarm "deps" (dependency (importJSON lockfile));
+          deps = linkFarm "deps" depsFromLockData;
         in
-        runCommand name { DENO_DIR = "deno"; } ''
+        runCommand name ({ DENO_DIR = "deno"; } // args) ''
           mkdir -p "$DENO_DIR" "$out/bin"
-          ln -s "${deps lockfile}" "$DENO_DIR/deps"
+          ln -s "${deps}" "$DENO_DIR/deps"
 
-          ${deno}/bin/deno compile --unstable --lock="${lockfile}" --cached-only -o "$out/bin/$name" "${entrypoint}"
+          ${deno}/bin/deno compile --unstable --lock="$lockfile" --cached-only -o "$out/bin/$name" "$entrypoint"
         '';
     in
     {

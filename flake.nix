1d8bb5baeed92387891331f7d85bf9e5b70e6d08
Peter Esselius <peter.esselius@pagero.com>
2021-04-16 08:44:48 +0200

    scrub scrub scrub

--- flake.nix
+++ flake.nix
@@ -7,23 +7,25 @@
 
       mkDenoDrv = { name, lockfile, entrypoint }@args:
         let
+          inherit (builtins) split hashString;
           inherit (pkgs) lib fetchurl linkFarm writeText runCommand deno;
           inherit (pkgs.lib) elemAt flatten mapAttrsToList importJSON;
 
-          urlPart = url: elemAt (flatten (builtins.split "://([a-z0-9\.]*)" url));
-          artifactPath = url: "${urlPart url 0}/${urlPart url 1}/${builtins.hashString "sha256" (urlPart url 2)}";
-          file = name: content: { inherit name; path = content; };
+          urlPart = url: elemAt (flatten (split "://([a-z0-9\.]*)" url));
+          artifactPath = url: "${urlPart url 0}/${urlPart url 1}/${hashString "sha256" (urlPart url 2)}";
 
-          lockData = importJSON lockfile;
+          dep = url: sha256: [
+            {
+              name = artifactPath url;
+              path = fetchurl { inherit url sha256; };
+            }
+            {
+              name = (artifactPath url) + ".metadata.json";
+              path = writeText "metadata.json" ''{"headers": {}, "url": ""}'';
+            }
+          ];
 
-          depsFromLockData = flatten (mapAttrsToList
-            (url: sha256: [
-              (file "${artifactPath url}" (fetchurl { inherit url sha256; }))
-              (file "${artifactPath url}.metadata.json" (writeText "metadata.json" ''{"headers": {}, "url": ""}''))
-            ])
-            lockData);
-
-          deps = linkFarm "deps" depsFromLockData;
+          deps = linkFarm "deps" (flatten (mapAttrsToList dep (importJSON lockfile)));
         in
         runCommand name ({ DENO_DIR = "deno"; } // args) ''
           mkdir -p "$DENO_DIR" "$out/bin"
